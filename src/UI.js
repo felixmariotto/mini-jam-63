34e688f5f2c21990e82c7c9f4ccccd87ab5cec89
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-18 13:52:27 +0200

    update game viewport to be square

--- src/UI.js
+++ src/UI.js
@@ -5,6 +5,8 @@ import GameManager from './GameManager.js';
 
 const domHomescreen = document.querySelector('#homescreen');
 const domStartBtn = document.querySelector('#start-btn');
+
+const domSquareContainer = document.querySelector('#game-square-container');
 const domScenes = document.querySelector('#scenes');
 
 let isGameOn = false;
@@ -35,14 +37,16 @@ window.addEventListener('keydown', (e) => {
 
 })
 
-window.addEventListener('mousemove', (e) => {
+domSquareContainer.addEventListener('mousemove', (e) => {
+
+	const rect = domSquareContainer.getBoundingClientRect();
 
 	const pos = {
-		x: e.x / window.innerWidth,
-		y: e.y / window.innerHeight
+		x: ( e.x - rect.left ) / rect.width,
+		y: ( e.y - rect.top ) / rect.height
 	};
 
-	domScenes.style.transform = `translate( ${ (1 - pos.x) * -100 }vw, ${ (1 - pos.y) * -100 }vh )`
+	domScenes.style.transform = `translate( ${ (1 - pos.x) * -100 }%, ${ (1 - pos.y) * -100 }% )`
 
 })
 
