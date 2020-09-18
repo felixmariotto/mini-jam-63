97e425b1a7ba08b1ef8696289af3ba776584b8ac
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-18 14:15:57 +0200

    uninstall planck.js, install matter.js

--- src/physics/World.js
+++ src/physics/World.js
@@ -1,15 +1,9 @@
 
-import planck from 'planck-js';
-
-//
-
-const world = planck.World();
-
 //
 
 function animate( deltaTime ) {
 
-	world.step( deltaTime / 1000 );
+	console.log('animate physics')
 
 }
 
