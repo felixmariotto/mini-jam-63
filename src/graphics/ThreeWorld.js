95e75d5381a3885130816f31b6c1702787fd20f0
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-18 12:20:25 +0200

    create 4 canvases with masks

--- src/graphics/ThreeWorld.js
+++ src/graphics/ThreeWorld.js
@@ -1,4 +1,16 @@
 
+const domLeftScene = document.querySelector('.scene-mask.left canvas');
+const domTopScene = document.querySelector('.scene-mask.top canvas');
+const domRightScene = document.querySelector('.scene-mask.right canvas');
+const domBottomScene = document.querySelector('.scene-mask.bottom canvas');
+
+domLeftScene.style.backgroundColor = "#00ff00";
+domTopScene.style.backgroundColor = "#ffff00";
+domRightScene.style.backgroundColor = "#00ffff";
+domBottomScene.style.backgroundColor = "#ff0000";
+
+//
+
 function animate( deltaTime ) {
 
 	console.log( deltaTime )
